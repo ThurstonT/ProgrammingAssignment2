b69e01920c688f431fc27e3cf935a387cc99a67d
ThurstonT <thurston.t@gmail.com>
2015-10-24 00:52:07 -0500

    performed additioanl testing and updated comments in file

--- cachematrix.R
+++ cachematrix.R
@@ -4,22 +4,29 @@
 ##  These functions work with the understanding that the matrix is a square matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
-#
+##This function contains four functions. The functions are set, get, setmatrix, getmatrix
+##  the Set function sets x to the matrix input to the makeCacheMatrix.  x is saved in the Gloabl Environment     
         mm <- NULL
         set <- function(y) {
             x <<- y
             mm <<- NULL
         }
-        get <- function() x
+        
+        get <- function() x     # get returns the matrix from the global environment.        
+        
+        ## setmatrix stores the matrix in the global environment
         setmatrix <- function(matrix) mm <<- matrix
-        getmatrix <- function() mm
-        list(set = set, get = get,
+        
+        getmatrix <- function() mm    ## getmatrix retrieves the matrix from the global environment
+        list(set = set, get = get,    ## return a list of functions. 
              setmatrix = setmatrix,
              getmatrix = getmatrix)
     }
 
 
-## This is a function that 'caches' the inverse of the matrix in the global environment.
+## cacheSolve will invert the matrix is the matrix has not previously been inverted
+## the inverted matrix will be stored, 'cached' in the global environment and returned to the user.
+## 
 ## Once the inverse of the matrix is computed it is stored in the global environment.  This operation
 ## saves computational time when frequently computing the same matrix inversion
 ## the special programming about this function is the use of the <<- operator. 
@@ -27,13 +34,13 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
          mm <- x$getmatrix()
-        if(!is.null(mm)) {
-##            message("getting cached data")
-            return(mm)
+        if(!is.null(mm)) {    ##  check to see if the inverted matrix has been stored in the global environment
+            message("getting cached data")  ## this is a debug/status statement to ensure cached data is returned 
+            return(mm)                      ## exit the function here
         }
-        data <- x$get()
-        mm <- solve(data, ...)
-        x$setmatrix(mm)
+        data <- x$get()       ## if the inverse of the matrix had not previously been computed, compute it now
+        mm <- solve(data, ...)     ## the matrix is inverted with this command 
+        x$setmatrix(mm)            ## the inverted matrix is stored with this command 
         mm
     
     
